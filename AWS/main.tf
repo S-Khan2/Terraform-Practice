d4df25574016ec409277855eb4c05e5680a1e160
Sahl Khan <skg.skhan2@gmail.com>
2022-10-24 16:47:25 +0100

    Corrected lifecyle policy of the log group

--- AWS/main.tf
+++ AWS/main.tf
@@ -114,6 +114,6 @@ resource "aws_cloudwatch_log_group" "function_log_group" {
   name              = "/aws/lambda/${aws_lambda_function.lambda.function_name}"
   retention_in_days = 7
   lifecycle {
-    prevent_destroy = True
+    prevent_destroy = true
   }
 }
