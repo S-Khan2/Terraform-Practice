8364de6446907974ea25c69d583c447c6162cf0f
Sahl Khan <skg.skhan2@gmail.com>
2022-10-24 16:33:53 +0100

    Added S3 CSV created trigger to Lambda

--- AWS/main.tf
+++ AWS/main.tf
@@ -14,6 +14,27 @@ resource "aws_s3_bucket_public_access_block" "s3_block" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_notification" "csv_created" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".csv"
+  }
+  depends_on = [
+    aws_lambda_permission.bucket_invocation
+  ]
+}
+
+resource "aws_lambda_permission" "bucket_invocation" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.s3_bucket.arn
+}
+
 data "archive_file" "zip" {
   type        = "zip"
   source_file = "src/lambda.py"
